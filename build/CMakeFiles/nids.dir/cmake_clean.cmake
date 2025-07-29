ceb4d6a821b19af9e68367c84d35d956fefa43df
president-xd <lasharimohsin19@gmail.com>
2025-07-29 13:10:26 -0400

    utils directory created

---
+++ build/CMakeFiles/nids.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nids.dir/link.d"
+  "CMakeFiles/nids.dir/src/main.cpp.o"
+  "CMakeFiles/nids.dir/src/main.cpp.o.d"
+  "CMakeFiles/nids.dir/src/parsers/ethernet.cpp.o"
+  "CMakeFiles/nids.dir/src/parsers/ethernet.cpp.o.d"
+  "nids"
+  "nids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/nids.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for nids.
+# This may be replaced when dependencies are built.
