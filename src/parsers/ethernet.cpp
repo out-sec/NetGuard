3b6a1e26a3d5fda922cfc0de338236d3ff1572ff
president-xd <lasharimohsin19@gmail.com>
2025-07-30 05:18:34 -0400

    ADDITIONS: FIXES, DECODERS, FORMATTERS added to make the output more interesting and well maintained

--- src/parsers/ethernet.cpp
+++ src/parsers/ethernet.cpp
@@ -11,14 +11,61 @@
 
 namespace parsers {
 
+// std::string EthernetHeader::to_string() const {
+//     std::ostringstream oss;
+//     oss << "Ethernet Frame:\n"
+//         << "  Destination MAC: " << utils::mac_to_string(dest_mac) << "\n"
+//         << "  Source MAC:      " << utils::mac_to_string(src_mac) << "\n"
+//         << "  EtherType:       0x" << std::hex << ntohs(eth_type);
+//     return oss.str();
+// }
+
 std::string EthernetHeader::to_string() const {
     std::ostringstream oss;
     oss << "Ethernet Frame:\n"
-        << "  Destination MAC: " << utils::mac_to_string(dest_mac) << "\n"
-        << "  Source MAC:      " << utils::mac_to_string(src_mac) << "\n"
-        << "  EtherType:       0x" << std::hex << ntohs(eth_type);
+        << "  Destination MAC: " << dest_mac_str << "\n"
+        << "  Source MAC:      " << src_mac_str << "\n"
+        << "  EtherType:       " << eth_type_str << " (" << eth_proto << ")";
     return oss.str();
 }
+
+EthernetHeader parse_ethernet_header(const uint8_t* buffer, ssize_t length) {
+    EthernetHeader eth;
+
+    if (!utils::is_valid_ethernet_frame(buffer, length))
+        return eth;
+
+    // Reconstruct the EthernetHeader
+    std::memcpy(eth.dest_mac, buffer, 6);         // First 6 bytes: dest MAC
+    std::memcpy(eth.src_mac, buffer + 6, 6);      // Next 6 bytes: source MAC
+    eth.eth_type = ntohs(*reinterpret_cast<const uint16_t*>(buffer + 12));  // EtherType (2 bytes)
+
+    eth.payload = buffer + 14;
+    eth.payload_len = length - 14;
+
+    // Assign readable strings for display/logging/debugging
+    eth.dest_mac_str = utils::mac_to_string(eth.dest_mac);
+    eth.src_mac_str  = utils::mac_to_string(eth.src_mac);
+    eth.eth_type_str = "0x" + utils::to_hex(eth.eth_type, 4);
+
+    switch (eth.eth_type) {
+        case 0x0800:
+            eth.eth_proto = "IPv4";
+            break;
+        case 0x0806:
+            eth.eth_proto = "ARP";
+            break;
+        case 0x86DD:
+            eth.eth_proto = "IPv6";
+            break;
+        default:
+            eth.eth_proto = "Unknown";
+            break;
+    }
+
+    return eth;
+}
+
 
 int create_raw_socket() {
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
@@ -43,50 +90,21 @@ int create_raw_socket() {
 
 
 bool parse_ethernet_frame(const uint8_t* buffer, ssize_t length) {
-    if (!utils::is_valid_ethernet_frame(buffer, length))
-        return false;
-
-    EthernetHeader eth;
-
-    // Use utility functions to extract fields
-    auto dest_mac_str = utils::get_dest_mac(buffer, length);
-    auto src_mac_str  = utils::get_src_mac(buffer, length);
-    auto eth_type_opt = utils::get_ethertype(buffer, length);
-
-    if (!dest_mac_str || !src_mac_str || !eth_type_opt)
-        return false;
-
-    // Reconstruct the EthernetHeader
-    std::memcpy(eth.dest_mac, buffer, 6);
-    std::memcpy(eth.src_mac, buffer + 6, 6);
-    eth.eth_type = htons(*eth_type_opt); // Store in network byte order
-
-    eth.payload = buffer + 14;
-    eth.payload_len = length - 14;
+    EthernetHeader eth = parse_ethernet_header(buffer, length);
+    if (eth.eth_proto == "Unknown") return false;
 
     std::cout << eth.to_string() << '\n';
-    // std::cout << eth.payload_as_hex() << "\n";
 
-    uint16_t eth_type = ntohs(eth.eth_type);
-
-
-
-    switch (eth_type) {
-        case 0x0800: // IPv4
-            std::cout << "  Ethernet Protocol: IPv4\n";
-            // TODO: parse_ipv4(payload, payload_len);
-            parsers::parse_ipv4_packet(eth.payload, eth.payload_len);
-            break;
-        case 0x0806: // ARP
-            std::cout << "  Ethernet Protocol: ARP\n";
-            break;
-        case 0x86DD: // IPv6
-            std::cout << "  Ethernet Protocol: IPv6\n";
-            break;
-        default:
-            std::cout << "  Unknown or unsupported EtherType\n";
-            break;
+    if (eth.eth_proto == "IPv4") {
+        std::cout << "  Ethernet Protocol: IPv4\n";
+        // parsers::parse_ipv4_packet(eth.payload, eth.payload_len);
+    } else if (eth.eth_proto == "ARP") {
+        std::cout << "  Ethernet Protocol: ARP\n";
+    } else if (eth.eth_proto == "IPv6") {
+        std::cout << "  Ethernet Protocol: IPv6\n";
     }
+
+    return true;
 
     // if (eth.payload_len > 0)
     //     std::cout << eth.payload_as_hex() << "\n";
