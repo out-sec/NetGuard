a199545ee48c86c6267d64a1a515c9bbbbb85cdc
president-xd <lasharimohsin19@gmail.com>
2025-08-01 21:03:19 -0400

    MODIFIED: CHECK RFC 826 HARDWARE_TYPE FIX

--- include/parsers/arp.h
+++ include/parsers/arp.h
@@ -20,10 +20,17 @@ namespace parsers {
         std::vector<uint8_t> target_mac;  // Target's MAC address
         std::string target_ip;       // Target's IP address
 
+        std::string hardware_type_str;  // String representation of hardware type
+        std::string protocol_type_str;  // String representation of protocol type
+
     };
 
     // Parser Function to extract ARP Header from a buffer
     ARPHeader parse_arp_header(const uint8_t* buffer, ssize_t length);
+
+    std::string hardware_type_to_string(uint16_t type);
+
+    std::string protocol_type_to_string(uint16_t proto);
 
 } // namespace parsers
 
